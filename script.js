ef2bd2d0b41cedb4ccc70fdf82250ffc726b49a7
Juan-D07 <dominguezjuanmi2207@gmail.com>
2026-09-18 13:01:56 +0200

    segundo commit

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hello, World!");
+var greeting = "Welcome to the JavaScript world!";
+
+confirm(greeting);
+prompt("Please enter your name:", "Your name here");
+alert("Thank you for visiting!");
+console.log("Goodbye!");
