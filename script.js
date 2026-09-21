ed5e2a5f2622d227328861bc4bac051148d9250d
Juan-D07 <dominguezjuanmi2207@gmail.com>
2026-09-21 13:27:36 +0200

    segundo commit

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ console.log("Hello, World!");
 var greeting = "Welcome to the JavaScript world!";
 
 confirm(greeting);
-prompt("Please enter your name:", "Your name here");
-alert("Thank you for visiting!");
+let xd =prompt("Please enter your name:", "Your name here");
+alert("Thank you for visiting, " + xd + "!");
 console.log("Goodbye!");
