6540ce24462964e32cb7d27a1bdd6810ab9e37c8
Juan-D07 <dominguezjuanmi2207@gmail.com>
2026-09-22 13:05:30 +0200

    tercer commit

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-console.log("Hello, World!");
-var greeting = "Welcome to the JavaScript world!";
 
-confirm(greeting);
-let xd =prompt("Please enter your name:", "Your name here");
-alert("Thank you for visiting, " + xd + "!");
-console.log("Goodbye!");
+
+// let operacion =prompt("selecciona tu operacion por su numero \n 1. Suma \n 2. Substracción \n 3. Multiplicación \n 4. Division \n 6. Potencia", "Escribe solo el numero");
+// let num1 = parseFloat(prompt("Primer Numero","Escribe solo el numero"));
+// let num2 =parseFloat(prompt("Segundo Numero","Escribe solo el numero"));
+
+// let suma = (x1,x2) => x1+x2;
+// let resta = (x1,x2) => x1-x2;
+// let multi = (x1,x2) => x1*x2;
+// let divi = (x1,x2) => x1/x2;
+// let xd = (x1,x2) => ""+num1 +num2;
+// let powerof = (x1,x2) => x1**x2;
+
+
+// switch (operacion)  {
+//     case "1":
+//         alert(suma(num1,num2));
+//         break
+//     case "2":
+//         alert(resta(num1,num2));
+//         break
+//     case "3":
+//         alert(multi(num1,num2));
+//         break
+//     case "4":
+//         alert(divi(num1,num2));
+//         break
+//     case "5":
+//         alert(xd(num1,num2));
+//         break
+//     case "6":
+//         alert(powerof(num1,num2));
+//         break
+//     default:
+//         alert("Error en la entrega, vuelve a intenar")
+// }
